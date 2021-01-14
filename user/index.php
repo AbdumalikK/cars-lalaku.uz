b5c8fd795d42d94fab8ae869272455ce2fdf633d
Abdumalik Karimov <39477628+AbdumalikK@users.noreply.github.com>
2021-01-14 19:49:13 +0500

    changes

---
+++ user/index.php
@@ -0,0 +1,29 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>LaLaKoo Cars</title>
+    <!-- Styles css-->
+    <link rel="stylesheet" href="styles/source/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/css/style.css">
+    <!-- <link rel="stylesheet" href="styles/source/all.min.css"> -->
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <!-- JS Files -->
+    <script src="styles/source/jquery.min.js"></script>
+    <script src="styles/source/bootstrap.min.js"></script>
+    <script src="styles/source/popper.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4">Kirish</div>
+        <div class="col-4">Ro'yxatdan o'tish</div>
+        <div class="col-4">Chiqish</div>
+    </div>
+</div>
+
+</body>
+</html>
